048eb42f8fa978a675f9eb358cc924f26f63ccdd
Tal Schuster <talschuster@gmail.com>
2017-03-30 06:15:41 -0700

    mend

---
+++ knn.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def knn(vector, matrix, k=10):
+    """
+    Finds the k-nearest rows in the matrix with comparison to the vector.
+    Use the cosine similarity as a distance metric.
+
+    Arguments:
+    vector -- A D dimensional vector
+    matrix -- V x D dimensional numpy matrix.
+
+    Return:
+    nearest_idx -- A numpy vector consists of the rows indices of the k-nearest neighbors in the matrix
+    """
+
+    nearest_idx = []
+
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+    return nearest_idx
+
+def test_knn():
+    """
+    Use this space to test your knn implementation by running:
+        python knn.py
+    This function will not be called by the autograder, nor will
+    your tests be graded.
+    """
+    print "Running your tests..."
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+if __name__ == "__main__":
+    test_knn()
+
+
