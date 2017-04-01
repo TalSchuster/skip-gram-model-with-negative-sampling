d7617a6794e31c11523a9d6f96ef2adfd0bc78c0
Tal Schuster <talschuster@gmail.com>
2017-04-01 07:50:21 -0700

    implemented sgd

--- sgd.py
+++ sgd.py
@@ -84,9 +84,10 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
         # You might want to print the progress every few iterations.
 
         cost = None
-        ### YOUR CODE HERE
-        raise NotImplementedError
-        ### END YOUR CODE
+        
+        cost, grad = f(x)
+        x -= step * grad
+        x = postprocessing(x)
 
         if iter % PRINT_EVERY == 0:
             if not expcost:
@@ -138,4 +139,4 @@ def your_sanity_checks():
 
 if __name__ == "__main__":
     sanity_check()
-    your_sanity_checks()
+    #your_sanity_checks()
